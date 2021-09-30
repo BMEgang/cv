2f5478c743a6285fec1c185f88c23ad313ac1827
BMEgang <hgszuuob@gmail.com>
2021-10-01 06:57:16 +0800

    for the STL library implementation

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "DTString.h"
+
+using namespace std;
+using namespace DTLib;
+
+int main()
+{
+    String s = "D.T.Software";
+
+    cout << "Hello: " << s.str() << endl;
+
+    return 0;
+}
